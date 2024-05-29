9083ed52f12f540c3147d1dffdeb17d603e5a501
RicardoAcostaArambula <acostaricardo463@gmail.com>
2024-05-28 18:54:41 -0600

    Solves issue of adding to the schedule and introduces better formating

--- home.py
+++ home.py
@@ -6,7 +6,7 @@ def get_days()-> list:
 def get_times()->list:
     return ["7:30am - 8:20 am", "8:30 am - 9:20 am", "9:30 am - 10:20 am",
             "9:00 am - 10:20 am", "10:30 am - 11:20 am", "10:30 am - 11:50 am", 
-            "11:30 am - 12:20", "12:30 pm - 1:20 pm", "12:00 pm - 1:20 pm"
+            "11:30 am - 12:20", "12:30 pm - 1:20 pm", "12:00 pm - 1:20 pm",
             "1:30 pm - 2:20 pm","1:30 - 2:50 pm", "2:30 - 3:20 pm", 
             "3:00 pm - 4:20 pm", "4:30 pm - 5:50 pm", "4:30 pm - 7:20 pm",
             "6:00 pm - 7:20 pm", "6:00 pm - 8:50 pm", "7:30 am - 8:50 am"]
@@ -19,16 +19,11 @@ def get_final_time(day: str, time: str) -> str:
             line_list = line.strip().split(',')
             days = line_list[0]
             times = line_list[1]
-            # st.write(f"{days} - {day} | {times} - {time}")
-            # st.write((days == day) and (times == time))
             if days == day and times == time:
-                # st.write(line_list[0])
-                # st.write(line_list[1])
-                # st.write(line_list[2])
-                st.session_state.schedule.append(line_list)
-
+                return line_list[2]
+    return "Time not found"
 def display_schedule():
-    col1, col2, col3, col4 = st.columns(4)
+    col1, col2, col3, col4, col5= st.columns(5)
     with col1:
         st.write("Days")
     with col2:
@@ -37,20 +32,24 @@ def display_schedule():
         st.write("Final time")
     with col4:
         st.write("Update")
-    col5, col6, col7, col8 = st.columns(4)
+    with col5:
+        st.write("Add to Calendar")
+    st.divider()
+    col6, col7, col8, col9, col10 = st.columns(5)
     index = 0
-    for line in st.session_state.schedule:
-        st.write(line)
-        # with col5:
-        #     st.write(days)
-        # with col6:
-        #     st.write(time)
-        # with col7:
-        #     st.write(final)
-        st.divider()
-        # with col8:
-        #     delete = st.button("Delete", key=f"index-{index}")
-        #     index+=1
+    for line in st.session_state['schedule']:
+        days, time, final = line
+        with col6:
+            st.write(days)
+        with col7:
+            st.write(time)
+        with col8:
+            st.write(final)
+        with col9:
+            delete = st.button("Delete", key=f"index-{index}")
+        with col10:
+            add_to_calendar = st.button("Add", key=f"add-{index}")
+        index+=1
             
     
 st.header("Welcome to the place where you will find your Final schedule in a pleasent way")
@@ -59,30 +58,37 @@ st.title("Select the days and time you have your class to see your final test sc
 
 
 class_day_list = get_days()
-days = st.selectbox(
-    "Select the your class days",
-    class_day_list, 
-    placeholder="Day(s)"
-)
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    days = st.selectbox(
+        "Select the your class days",
+        class_day_list, 
+        placeholder="Day(s)"
+    )
 class_time_list = get_times()
-time = st.selectbox(
-    "Select the time of your class",
-    class_time_list,
-    placeholder="Time"
-)
+with col2:
+    time = st.selectbox(
+        "Select the time of your class",
+        class_time_list,
+        placeholder="Time"
+    )
 if 'schedule' not in st.session_state:
-    st.session_state.schedule = []
-st.session_state.schedule.append((days, time))
+    st.session_state['schedule'] = []
+#st.session_state['schedule'].append([days, time])
 
 if 'add' not in st.session_state:
     st.session_state.add = False
-
-add = st.button("Add to schedule", key="Add")
+with col3:
+    add = st.button("Add to schedule", key="Add")
 
 if add:
     final_time = get_final_time(days, time)
-    st.session_state.schedule.append((days, time, final_time))
-    display_schedule()
+    if final_time == "Time not found":
+        st.error("Time was not found, please select a valid day and time for your class")
+    else:
+        st.session_state['schedule'].append([days, time, final_time])
+        display_schedule()
     st.session_state.add = False
 
 
